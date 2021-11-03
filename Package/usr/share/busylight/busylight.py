ee02b83b7bdba71776f339fea67b119758cfcc6c
IT und Entwicklung - FG <account@it-und-entwicklung-fg.de>
2021-11-03 20:49:50 +0100

    Code gekürzt und aktualisiert

--- Package/usr/share/busylight/busylight.py
+++ Package/usr/share/busylight/busylight.py
@@ -1,21 +1,36 @@
 # Python script for the Python Demo Service
 
+from configparser import ConfigParser
+import os.path
+
 if __name__ == '__main__':
     import time
-    from bl import BusyLight
+    #from bl import BusyLight
     import systemd.daemon
 
     # Tell systemd that our service is ready
     systemd.daemon.notify('READY=1')
 
+    config_file = "/var/cache/busylight/config.conf"
+    config = ConfigParser()
+    config = {
+    "red": "0",
+    "green": "0",
+    "blue": "0"
+    }
+
     while True:
-        rgbfile = open("/var/cache/busylight/rgb", "r")
-        rgb=rgbfile.readlines()
-        if len(rgb) >= 3:
-            print("Red: " + rgb[0])
-            print("Green: " + rgb[1])
-            print("Blue: " + rgb[2])
-            #BusyLight(int(rgb[0]),int(rgb[1]),int(rgb[2]))
-        rgbfile.close()
+        if os.path.isfile(config_file):
+            config.read(config_file)
+            rgb = config["RGB"]
+            if len(rgb) >= 3:
+                print("Red: " + rgb["red"])
+                print("Green: " + rgb["green"])
+                print("Blue: " + rgb["blue"])
+                #BusyLight(int(rgb[0]),int(rgb[1]),int(rgb[2]))
+        else:
+            #Write the above sections to config.ini file
+            with open(config_file, 'w') as conf:
+                config.write(conf)
         time.sleep(2)
 
