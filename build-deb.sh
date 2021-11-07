e78491880457f6134d659268e60c78f557714964
IT und Entwicklung - FG <account@it-und-entwicklung-fg.de>
2021-11-07 14:32:02 +0100

    Updated build-script

--- build-deb.sh
+++ build-deb.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 cd ./Package
 debuild -b -kC3FBBEC27B2A17AE
-rm ./Package/debian/busylight.debhelper.log ./Package/debian/busylight.substvars ./Package/debian/files
-rm -r ./Package/debian/busylight ./Package/debian/.debhelper
